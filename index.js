4f04fe30d5cb7634437736faca3d515a61d35700
Jeffrey Neal <jeffneal11@gmail.com>
2023-02-16 10:19:39 -0600

    fully functioning

--- index.js
+++ index.js
@@ -8,34 +8,61 @@ require("dotenv").config();
 // sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
 console.log("started");
 function fetchLegoInfo(LegoObject, allPieces) {
-  console.log(LegoObject.setNumber);
+  // console.log(LegoObject.setNumber);
   axios.defaults.headers.common = {
-    "x-api-key": keys.leg1,
+    "x-api-key": keys.leg0,
+    // product_number: LegoObject.setNumber,
   };
   //https://services.slingshot.lego.com/api/v4/lego_historic_product_read/_search
   // /api/v4/lego_historic_product_read/_search
   // https://bricksandpieces.services.lego.com/api/v1/bricks/product/${LegoObject.setNumber} //OLD URL
+  // https://bricksandpieces.cs.services.lego.com/api/v1/bricks/product/60374?country=US&orderType=missing
   axios
     .get(
-      // `https://bricksandpieces.services.lego.com/api/v1/bricks/product/${LegoObject.setNumber}`,
-      "https://services.slingshot.lego.com/api/v4/lego_historic_product_read/_search",
+      `https://bricksandpieces.cs.services.lego.com/api/v1/bricks/product/${LegoObject.setNumber}`,
+      // `https://services.slingshot.lego.com/api/v4/lego_historic_product_read/_search`,
       {
-        // params: {
-        //   country: "US",
-        //   orderType: "buy",
-        // },
+        params: {
+          country: "US",
+          orderType: "missing",
+          // product_number: LegoObject.setNumber,
+          // _id: LegoObject.setNumber,
+        },
       }
     )
     .then((res) => {
-      console.log(res.data.hits.hits[0]._source.availability);
-      // handleLegoResponse(res.data, LegoObject, allPieces);
-      console.log(LegoObject.setNumber);
+      // console.log(res.data.bricks);
+      handleLegoResponse(res.data, LegoObject, allPieces);
+      // console.log(LegoObject.setNumber);
     });
 }
 
 function handleLegoResponse(responseData, pieceToLookFor, allPieces) {
   // console.log(responseData.bricks);
   const inStockPieces = responseData.bricks.filter((piece) => !piece.isSoldOut);
+  const outStockPieces = responseData.bricks.filter((piece) => piece.isSoldOut);
+  responseData.bricks.forEach((brick) => {
+    // console.log(brick);
+    if (pieceToLookFor.parts.includes(brick.itemNumber)) {
+      // console.log(brick);
+      !brick.isSoldOut &&
+        console.log(`${brick.colorFamily} ${brick.description} is available!`);
+      brick.isSoldOut &&
+        console.log(
+          `${brick.colorFamily} ${brick.description} is NOT available`
+        );
+    }
+  });
+  // inStockPieces.forEach((part) => {
+  //   // console.log(part.description, part.colorFamily, part.designId);
+  //   LegoObject.parts.forEach(wantedPart =>{
+  //     if(part.itemNumber == wantedPart){
+  //       console.log(wantedPart)
+  //     }
+  //   })
+
+  //   // console.log(part);
+  // });
   // console.log(inStockPieces);
   // const theChosenPiece = inStockPieces.filter(
   //   (piece) => piece.itemNumber === pieceToLookFor.itemNumber
@@ -84,10 +111,33 @@ function fetchPiecesToLookFor() {
   let rawdata = fs.readFileSync("pieces.json");
   return JSON.parse(rawdata);
 }
-const piecesToSearchFor = [{ setNumber: "4411" }];
-cron.schedule("*/1 * * * *", () => {
+//11027
+//11030
+
+const piecesToSearchFor = [{ setNumber: 60374, parts: [6430373] }];
+cron.schedule("*/5 * * * *", () => {
   //const piecesToSearchFor = fetchPiecesToLookFor();
   console.log("calling API...");
 
   piecesToSearchFor.forEach((piece) => fetchLegoInfo(piece, piecesToSearchFor));
 });
+
+/**
+ * {
+  color: '',
+  colorFamily: 'Tr. Opalescence',
+  description: 'PLATE 1X1 ROUND',
+  designId: 34823, // part number
+  isIPElement: false,
+  imageUrl: 'https://www.lego.com/cdn/product-assets/element.img.lod5photo.192x192/6430373.jpg',
+  itemNumber: 6430373,  //element id
+  itemQuantity: 4,
+  price: { amount: 0, currency: '' },
+  maxAmount: 4,
+  isAvailable: true,
+  unavailableReason: null,
+  isSoldOut: true,
+  category: 'Plates, Special Circles And Angles',
+  materialType: 'ELEMENT'
+}
+ */
